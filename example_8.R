87fece6592d003ebfed92b1a91b92f08132176cf
richelbilderbeek <richel@richelbilderbeek.nl>
2020-02-28 20:39:25 +0100

    Use of 'pir_save'

--- example_8.R
+++ example_8.R
@@ -1,7 +1,6 @@
 # Works under Linux and MacOS only
 
 library(pirouette)
-suppressMessages(library(ggplot2))
 
 ################################################################################
 # Constants
@@ -33,6 +32,9 @@ pir_out <- pir_run(
   pir_params = pir_params
 )
 
+################################################################################
+# Save results
+################################################################################
 pir_save(
   phylogeny = phylogeny,
   pir_params = pir_params,
